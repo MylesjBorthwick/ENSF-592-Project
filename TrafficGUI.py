bfaa14b7c99204130bfe0fc2e184e1921065bd03
Myles Borthwick <64812046+MylesjBorthwick@users.noreply.github.com>
2020-07-23 02:02:19 -0600

    Update

--- TrafficGUI.py
+++ TrafficGUI.py
@@ -450,7 +450,7 @@ class TrafficApp:
 
         else:
             self.msg_box.delete("1.0","end")
-            self.msg_box.insert(tk.END, "Error:"+"\n"+"Please"+"\n"+"select"+"\n"+"a"+"\n"+"Traffic"+"\n"+"Statistic")
+            self.msg_box.insert(tk.END, "Error:"+"\n"+"Please"+"\n"+"select "+"a"+"\n"+"Traffic"+"\n"+"Statistic")
             return [-1,-1,-1]
             
                 
